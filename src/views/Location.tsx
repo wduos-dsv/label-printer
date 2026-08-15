e48542c25c5f0e5ec8af30409f931250daa13b19
Wendel Santos - DSV <wendel.santos1@dsv.com>
2026-08-14 21:24:28 -0300

    Created fontSize option on Location.tsx

--- src/views/Location.tsx
+++ src/views/Location.tsx
@@ -8,6 +8,7 @@ interface PrinterInfo {
 export default function Location({ printerPort, printerIP }: PrinterInfo) {
   const [positionCode, setPositionCode] = useState("");
   const [includeQR, setIncludeQR] = useState(true);
+  const [fontSize, setFontSize] = useState(135);
 
   const [printStatus, setPrintStatus] = useState<
     "none" | "success" | "error" | "awaiting"
@@ -17,12 +18,14 @@ export default function Location({ printerPort, printerIP }: PrinterInfo) {
   const handlePrint = async () => {
     setPrintStatus("awaiting");
 
+    console.log(fontSize);
     try {
       const config = {
         ip: printerIP,
         port: printerPort,
         position: positionCode.trim(),
         hasQR: includeQR,
+        fontSize: fontSize,
       };
 
       // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -52,7 +55,7 @@ export default function Location({ printerPort, printerIP }: PrinterInfo) {
       <small className="view-subtitle">Código da posição</small>
       <input
         type="text"
-        maxLength={9}
+        maxLength={11}
         onChange={(e) => {
           setPositionCode(e.target.value.toUpperCase());
         }}
@@ -64,6 +67,18 @@ export default function Location({ printerPort, printerIP }: PrinterInfo) {
         checked={includeQR}
         onChange={() => setIncludeQR(!includeQR)}
       />
+
+      <small className="view-subtitle">Tamanho da fonte</small>
+      <select
+        value={String(fontSize)}
+        onChange={(e) => setFontSize(parseInt(e.target.value))}
+      >
+        <option value="135">135</option>
+        <option value="125">125</option>
+        <option value="115">115</option>
+        <option value="105">105</option>
+        <option value="95">95</option>
+      </select>
 
       <button
         className={
